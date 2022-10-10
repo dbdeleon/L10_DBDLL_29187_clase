cb3e98e75a79c65d64eac11fd33f8c155452a288
Darsy Betzabé De León López <dbdeleon@url.edu.gt>
2022-10-10 13:37:28 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace L10_DBDLL_29187_clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio LAB 10 - AL CUADRADO -");
+            Console.WriteLine(alCuadrado(10));
+            int segundoResutado = alCuadrado(5);
+            Console.WriteLine(segundoResutado);
+        }
+
+        public static int alCuadrado(int numero)
+        {
+            int resultado = numero * numero;
+            return resultado;
+        }   
+
+
+
+        
+    }
+}
